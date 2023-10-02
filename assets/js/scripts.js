3c84ecae0c28b0aca9d64c6be3087548e27fb2be
andreidrobota <>
2023-10-02 07:14:43 +0000

    Add play and increment scores functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,21 +11,52 @@ document.addEventListener("DOMContentLoaded", function () {
     // how to determine the winner
     function determineWinner(playerChoice, compChoice) {
         if (playerChoice === compChoice) {
-            document.getElementById('result').innerText = 'You tied';
+            document.getElementById('result').innerText = 'tied';
         } else if (
             (playerChoice === 'rock' && compChoice === 'scissors') ||
             (playerChoice === 'paper' && compChoice === 'rock') ||
             (playerChoice === 'scissors' && compChoice === 'paper')
         ) {
-            document.getElementById('result').innerText = "You won! :)";
+            document.getElementById('result').innerText = "won! :)";
             increasePlayerScore();
         } else {
-            document.getElementById('result').innerText = "You lost! :(";
+            document.getElementById('result').innerText = "lost! :(";
             increaseCompScore();
         }
     }
 
+    function displayChoices(playerChoice, compChoice) {
+
+        document.getElementById('player-choice').innerText = playerChoice;
+        document.getElementById('comp-choice').innerText = compChoice;
+    }
 
 
+    // handle the button clicks 
 
-};
+    function play(playerChoice) {
+        let computer = compChoice();
+        let result = determineWinner(playerChoice, computer);
+
+        displayChoices(playerChoice, computer);
+    }
+
+    // Add click event listeners to buttons 
+
+    document.getElementById('rock').addEventListener('click', () => play('rock'));
+    document.getElementById('paper').addEventListener('click', () => play('paper'));
+    document.getElementById('scissors').addEventListener('click', () => play('scissors'));
+
+
+    function increasePlayerScore() {
+        let incrementP = parseInt(document.getElementById('player-score').innerText);
+        ++incrementP;
+        document.getElementById('player-score').innerText = incrementP;
+    }
+
+    function increaseCompScore() {
+        let incrementC = parseInt(document.getElementById('comp-score').innerText);
+        ++incrementC;
+        document.getElementById('comp-score').innerText = incrementC;
+    }
+});
